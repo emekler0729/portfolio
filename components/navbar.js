3c3e79922cf983f6c03832109b20a2814e4f862e
Eduard <emekler0729@gmail.com>
2021-08-25 15:58:41 -0500

    added navbar as a component

---
+++ components/navbar.js
@@ -0,0 +1,28 @@
+class Navbar extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="container-fluid px-md-5">
+                <a href="index.html" class="navbar-brand">Eduard Mekler</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-md-end" id="navbarNav">
+                    <div class="navbar-nav">
+                        <a href="#services" class="nav-link">Services</a>
+                        <a href="#projects" class="nav-link mx-md-2">Projects</a>
+                        <a href="#contact" class="nav-link mx-md-2">Contact</a>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        this.classList.add('navbar', 'navbar-expand-md', 'navbar-light', 'bg-white')
+    }
+}
+
+customElements.define('portfolio-nav', Navbar);
